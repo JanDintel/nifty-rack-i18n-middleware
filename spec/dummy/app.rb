d541b0f2ae531ef6b461b8e461943fc7de2d8d4d
Jan van der Pas <janvanderpas@gmail.com>
2015-06-11 01:26:37 +0200

    [Dummy] Add the 'nifty-rack-i18n-middleware' gem to the Gemfile

--- spec/dummy/app.rb
+++ spec/dummy/app.rb
@@ -1,5 +1,9 @@
 require_relative './config/environment'
 require 'sinatra'
+require 'nifty/rack/i18n/middleware'
+
+use Rack::Lint
+use Nifty::Rack::I18n::Middleware
 
 get '/' do
   JSON.generate({ current_locale: I18n.locale.to_s, message: I18n.t('hello_world') })
